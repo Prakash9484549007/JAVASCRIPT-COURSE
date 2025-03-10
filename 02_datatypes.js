843f861a6ccfd56edba7a4b6b35c807c4af5de35
Prakash9484549007 <lalchandanip595@gmail.com>
2025-03-11 00:00:38 +0530

    Hi

---
+++ 02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all JS code as newer version
+// alert("Hello World") we are using node.js not browser
+
+console.log(3 + 3)
+console.log("Hello World") 
+
+let name = "Prakash"
+let age = 18
+let isLoggedIn = false
+let temp = null
+
+console.log(temp)
+
+
+// number => 2 to the power of 53
+// bigint 
+// string => "Hello", 'World'
+// boolean => true, false
+// null => standalone value
+// undefined => empty value
+// symbol => unique value
+
+// object => complex data structure
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof "Prakash");
+console.log(typeof undefined);
+
+
+
